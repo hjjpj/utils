e21d7c6d28ba121794e274b0c38e86afba0e372f
hjjpj <41061459+hjjpj@users.noreply.github.com>
2018-07-11 12:04:21 +0000

    Update Test.java

--- CommonUtil/src/Test.java
+++ CommonUtil/src/Test.java
@@ -1,8 +1,8 @@
 public class Test {
 
   public static void main(String[] args){
-    System.err.println("Test...ddd");
-    System.err.println("Test...ddd");
+    System.err.println("Test...d2dd");
+    System.err.println("Test...dd2d");
   }
   
 }
