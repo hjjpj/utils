a8a1b27ec1e4aea0185e5b34ba57ffb69050b89c
a <a@118.37.15.199>
2018-07-11 21:08:00 +0900

    ㄴㄴㄴ

--- CommonUtil/src/Test.java
+++ CommonUtil/src/Test.java
@@ -1,13 +1,9 @@
 public class Test {
 
   public static void main(String[] args){
-<<<<<<< HEAD
     System.err.println("Test...dd2d");
     System.err.println("Test...ssddd");
-=======
-    System.err.println("Test...d2dd");
-    System.err.println("Test...dd2d");
->>>>>>> branch 'master' of https://github.com/hjjpj/utils.git
+
   }
   
 }
