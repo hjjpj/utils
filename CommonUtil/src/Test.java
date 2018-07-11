309b8f8b229c46be4de2f6b8433b1dd1a7ccc566
a <a@118.37.15.199>
2018-07-11 21:06:25 +0900

    재진 - 커밋

--- CommonUtil/src/Test.java
+++ CommonUtil/src/Test.java
@@ -1,8 +1,8 @@
 public class Test {
 
   public static void main(String[] args){
-    System.err.println("Test...ddd");
-    System.err.println("Test...ddd");
+    System.err.println("Test...dd2d");
+    System.err.println("Test...ssddd");
   }
   
 }
--- CommonUtil/src/Test2.java
+++ CommonUtil/src/Test2.java
@@ -1,6 +1,6 @@
 public class Test2 {
 
 	  public static void main(String[] args){
-		    System.err.println("Test..s.");
+		    System.err.println("Test.www.s.");
 		  }
 }
